dbb7085d4a6b0a87624e9ea28f40c8975565c2fd
bderashri <bansi.derashri@gmail.com>
2019-06-06 17:43:01 +0530

    sorting functions

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -21,8 +21,10 @@ class Panel extends Component{
           showMenu: false,
           IdeaClick:false,
           AssetClick:false,
-          StagesHighClick:false,
-          StagesLowClick:false,
+          NameDesc:false,
+          NameAsc:false,
+          DateRecentF:false,
+          DateOldestF:false,
           ideaList: [],
           assetList: [],
         }
@@ -31,15 +33,7 @@ class Panel extends Component{
         this.closeMenu = this.closeMenu.bind(this);
         this.IdeaClicked = this.IdeaClicked.bind(this);
         this.AssetClicked = this.AssetClicked.bind(this);
-        //this.CloseStagesHighClicked = this.CloseStagesHighClicked(this);
-        //this.CloseStagesLowClicked = this.CloseStagesLowClicked(this);
-        //this.sortArrayOfObjects= this.sortArrayOfObjects.bind(this);
-        this.sortMainline = this.sortMainline.bind(this);
-        this.sortIdea = this.sortIdea.bind(this);
-        this.sortAsset = this.sortAsset.bind(this);
-        this.sortHighAsset = this.sortHighAsset.bind(this);
-    
-       this.sortArray = this.sortArray.bind(this);
+        this.sortArray = this.sortArray.bind(this);
     }
 
 
@@ -56,124 +50,112 @@ class Panel extends Component{
     return ((x > y) ? -1 : ((x < y) ? 1 : 0));
    }
 
+   sortDate(a,b){
+       var date1 = new Date(a.date);
+      // alert(date1)
+       var date2 = new Date(b.date);
+       return ((date1 > date2) ? -1 : ((date1 < date2) ? 1 : 0))
+   }
+
+   rsortDate(a,b){
+        var date1 = new Date(a.date);
+        var date2 = new Date(b.date);
+        return ((date1 < date2) ? -1 : ((date1 > date2) ? 1 : 0))
+   }
+
    sortArray(){
+        var newList =[]
        if(this.IdeaClicked){
-        //alert("inside idea sort")
-        var newList = this.ideaArray;
-        newList.sort(this.sortMainline)
-        this.setState({
-            ideaList : newList
-        });
+            newList = this.ideaArray;
+            //newList.sort(this.sortMainline)
+            this.setState({
+                ideaList : newList
+            });
        }
 
        if(this.AssetClicked){
-       // alert("inside asset sort")
-        var newList = this.assetArray;
-        newList.sort(this.sortMainline)
-        this.setState({
-            assetList : newList
-        });
+            newList = this.assetArray;
+           // newList.sort(this.sortMainline)
+            this.setState({
+                assetList : newList
+            });
        }
        
-       if(this.state.StagesLowClick){
-            var newList = this.state.assetList;
-        newList.sort(this.sortMainline)
-        this.setState({
-            assetList : newList
-        });
+       if(this.state.NameAsc){
+            newList = this.assetArray;
+            newList.sort(this.sortMainline)
+            this.setState({
+                assetList : newList
+            });
 
-        newList = this.state.ideaList;
-        newList.sort(this.sortMainline)
-        this.setState({
-            ideaList : newList
-        });
+            newList = this.ideaArray;
+            newList.sort(this.sortMainline)
+            this.setState({
+                ideaList : newList
+            });
        }
-       if(this.state.StagesHighClick){
-            var newList = this.state.assetList;
+
+       if(this.state.NameDesc){
+            newList = this.assetArray;
             newList.sort(this.rsortMainline)
             this.setState({
                 assetList : newList
             });
 
-            newList = this.state.ideaList;
+            newList = this.ideaArray;
             newList.sort(this.rsortMainline)
             this.setState({
                 ideaList : newList
             });
+           
        }
+
+       if(this.state.DateOldestF){
+        newList = this.assetArray;
+        newList.sort(this.sortDate)
+        this.setState({
+            assetList : newList
+        });
+
+        newList = this.ideaArray;
+        newList.sort(this.sortDate)
+        this.setState({
+            ideaList : newList
+        });
+        
+    }
+        
+        if(this.state.DateRecentF){
+
+            newList = this.assetArray;
+            newList.sort(this.rsortDate)
+            this.setState({
+                assetList : newList
+            });
+    
+            newList = this.ideaArray;
+            newList.sort(this.rsortDate)
+            this.setState({
+                ideaList : newList
+            
+        });
+    }
 
     
    }
-    sortIdea()  {
-           alert("inside idea sort")
-            var newList = this.ideaArray;
-            newList.sort(this.sortMainline)
-            this.setState({
-                ideaList : newList
-            });
-        }
-
-    sortAsset(){
-       // alert("inside asset sort")
-        var newList = this.assetArray;
-        newList.sort(this.sortMainline)
-        this.setState({
-            assetList : newList
-        });
-    }
-            
-    sortHighAsset(){
-       // alert("set state done")
-        var newList = this.state.assetList;
-        newList.sort(this.rsortMainline)
-        this.setState({
-            assetList : newList
-        });
-
-        newList = this.state.ideaList;
-        newList.sort(this.rsortMainline)
-        this.setState({
-            ideaList : newList
-        });
-    }
-
-    sortLowAsset(){
-      //  alert("set state done")
-        var newList = this.state.assetList;
-        newList.sort(this.sortMainline)
-        this.setState({
-            assetList : newList
-        });
-
-        newList = this.state.ideaList;
-        newList.sort(this.sortMainline)
-        this.setState({
-            ideaList : newList
-        });
-    }
-
 
     IdeaClicked(event){
         event.preventDefault();
-        this.setState({ IdeaClick: true })/*,() => {
-            document.addEventListener('click', this.CloseIdea);
-          });*/
-        this.setState({ AssetClick: false })/*, () => {
-            document.removeEventListener('click', this.CloseAsset);
-          }); */
-        this.sortArray();
-        
+        this.setState({ IdeaClick: true })
+        this.setState({ AssetClick: false })
+        this.sortArray(); 
     }
 
  
     AssetClicked(event){
         event.preventDefault();
-        this.setState({ AssetClick: true })/*, () => {
-            document.addEventListener('click', this.CloseAsset);
-        });*/
-        this.setState({ IdeaClick: false })/*, () => {
-            document.removeEventListener('click', this.CloseIdea);
-          });  */
+        this.setState({ AssetClick: true })
+        this.setState({ IdeaClick: false })
         this.sortArray();
     }
 
@@ -192,43 +174,36 @@ class Panel extends Component{
       }
     
       
-    StagesHighClicked(event){
-        //event.preventDefault();
-        this.setState({StagesHighClick:true})
-        this.setState({StagesLowClick:false})
-       /*this.setState({StagesLowClick:false})/*,() => {
-            document.addEventListener('click', this.CloseStagesHighClicked);
-         });*/
-         this.sortArray();
+    NameDescending(event){
+        event.preventDefault()
+        this.setState({NameDesc:true})
+        this.setState({NameAsc:false})
+        this.sortArray();
 
     }
       
-    StagesLowClicked(){
-        this.setState({StagesLowClick:true})
-        this.setState({StagesHighClick:false})
+    NameAscending(event){
+        event.preventDefault()
+        this.setState({NameAsc:true})
+        this.setState({NameDesc:false})
         this.sortArray();
     }
-/*
 
-    CloseStagesHighClicked(){
-        
-         this.setState({StagesHighClicked:false}, ()=> {
-            document.removeEventListener('click', this.CloseStagesHighClicked)
-        });
-        
-        
+    DateRecentFirst(event){
+        event.preventDefault()
+        this.setState({DateRecentF:true});
+        this.setState({DateOldestF:false});
+        this.sortArray();
     }
 
-    CloseStagesLowClicked(){
-        this.setState({StagesLowClicked:false}, ()=> {
-            document.removeEventListener('click', this.CloseStagesLowClicked)
-        });
-    }*/
-       
+    DateOldestFirst(event){
+        event.preventDefault()
+        this.setState({DateOldestF:true});
+        this.setState({DateRecentF:false});
+        this.sortArray();
+    }
 
     render(){
-        
-        
         return(
             <div>
                <Container fluid="true"  >
@@ -270,11 +245,12 @@ class Panel extends Component{
                     ?(
                         <div className="dropDown" ref={(element) => {this.dropdownMenu = element;}}>
                         <ul >
-                            <li >New</li>
-                            <li>Relevance</li> 
-                            <li>Popularity</li>
-                            <li onClick={this.StagesLowClicked.bind(this)}> Stages(High to Low)</li>
-                            <li onClick={this.StagesHighClicked.bind(this)}>Stages(Low to High)</li>
+                            <li onClick={this.DateRecentFirst.bind(this)}>Date(Latest->Oldest)</li> 
+                            <li onClick={this.DateOldestFirst.bind(this)}>Date(Oldest->Latest)</li>
+                            <li onClick={this.NameDescending.bind(this)}>Name(A->Z)</li>
+                            <li onClick={this.NameAscending.bind(this)}>Name(Z->A)</li>
+                            <li> Progress(Ascedning)</li>
+                            <li> Progress(Descending</li>
                         </ul>
                     </div>
                     )
